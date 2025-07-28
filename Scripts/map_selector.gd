caf9d8c1475f0fd79c7aafbc60ff7516e8486e2b
Kamil Wolak <118762973+szachiszta@users.noreply.github.com>
2025-07-28 18:28:00 +0200

    fix layoutu
    teraz layout bardziej czytelny - ogólnie button raczej nie powinien mieć jako tła obrazku chyba ale na ten moment będzie w ten sposób - w miarę wygląda i działa odpowiednio

--- Scripts/map_selector.gd
+++ Scripts/map_selector.gd
@@ -6,30 +6,40 @@ func _ready():
 	var dir_path = "res://Scenes/Maps"
 	var dir = DirAccess.open(dir_path)
 
+	if dir == null:
+		print("Nie udało się otworzyć katalogu:", dir_path)
+		return
+
 	for file_name in dir.get_files():
 		if file_name.ends_with(".tscn"):
-			var base_name = file_name.get_basename()
-			var thumbnail_path = base_name + ".png"
-			print(thumbnail_path)
-			var button = Button.new()
-			button.text = base_name
-			# Ikonka dla buttona - narazie tak (placeholder powiedzmy)
-			var icon_texture_rect = TextureRect.new()
-			icon_texture_rect.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-			icon_texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED # 
-			var desired_icon_size = Vector2(32, 32) 
-			icon_texture_rect.custom_minimum_size = desired_icon_size
-
-			if FileAccess.file_exists(dir_path+ "/" + thumbnail_path):
-				var texture = load(dir_path+ "/" + thumbnail_path)
-				if texture:
-					icon_texture_rect.texture = texture
-				else:
-					print("Failed to load thumbnail texture")
-			else:
-				print("thumbnail doesn't exist")
-			button.add_child(icon_texture_rect)
-
 			var map_path_full = dir_path + "/" + file_name
+			var map_name = file_name.get_basename()
+
+			# KONTENER kafelka
+			var tile = VBoxContainer.new()
+			tile.size_flags_horizontal = Control.SIZE_FILL
+
+			# PRZYCISK lub placeholder pod miniaturkę
+			var button = Button.new()
+			button.custom_minimum_size = Vector2(0, 100)  
+			button.position = tile.position
+			# Wczytujemy miniaturę, jeśli istnieje
+			var thumbnail_path = dir_path + "/" + map_name + ".png"
+			if FileAccess.file_exists(thumbnail_path):
+				var texture = load(thumbnail_path)
+				if texture:
+					button.icon = texture
+					button.icon_alignment = HORIZONTAL_ALIGNMENT_CENTER  # Ikonka na środku
+					button.expand_icon = true  # Ikona wypełni przycisk
 			button.connect("pressed", func(): emit_signal("map_selected", map_path_full))
-			$GridContainer.add_child(button)
+			tile.add_child(button)
+
+			# NAZWA mapy
+			var label = Label.new()
+			label.text = map_name
+			label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+			label.autowrap_mode = TextServer.AUTOWRAP_WORD
+			tile.add_child(label)
+
+			# Dodajemy do GridContainer
+			$ScrollContainer/MarginContainer/GridContainer.add_child(tile)
