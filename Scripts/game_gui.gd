db8463a43418801a1c79348a631ad90a50837e62
Mori656 <marcelnosek@o2.pl>
2025-08-20 23:46:39 +0200

    Wyświetlanie banku

--- Scripts/game_gui.gd
+++ Scripts/game_gui.gd
@@ -6,8 +6,18 @@ extends Control
 @onready var oil_count = $PlayerResourcesContainer/Panel/OilCount
 @onready var coal_count = $PlayerResourcesContainer/Panel/CoalCount
 @onready var uran_count = $PlayerResourcesContainer/Panel/UranCount
+
+@onready var bank_wood_count = $BankResourceContainer/Panel/WoodCount
+@onready var bank_iron_count = $BankResourceContainer/Panel/IronCount
+@onready var bank_oil_count = $BankResourceContainer/Panel/OilCount
+@onready var bank_coal_count = $BankResourceContainer/Panel/CoalCount
+@onready var bank_uran_count = $BankResourceContainer/Panel/UranCount
+
 @onready var resources_container = $PlayerResourcesContainer
 @onready var resources_container_pos = resources_container.position
+
+@onready var bank_container = $BankResourceContainer
+@onready var bank_container_pos = bank_container.position
 
 @onready var dice1 = $DiceContainer/DiceButton/Dice1Sprite
 @onready var dice2 = $DiceContainer/DiceButton/Dice2Sprite
@@ -17,6 +27,7 @@ var k2 = 0
 var map_tiles = -1
 var resource_dict = {1:"wood", 2:"iron", 3:"oil", 4:"coal", 5:"uran"}
 @onready var resource_count = {"wood":wood_count, "iron":iron_count, "oil":oil_count, "coal":coal_count, "uran":uran_count}
+@onready var bank_resource_count = {"wood":bank_wood_count, "iron":bank_iron_count, "oil":bank_oil_count, "coal":bank_coal_count, "uran":bank_uran_count}
 
 func update_gui():
 	print("Uruchamiam update")
@@ -26,8 +37,10 @@ func update_gui():
 func update_player_resources():
 	print("Gracz ", multiplayer.get_unique_id(), " robi update")
 	var player_resources = inventory.get_player_resources(multiplayer.get_unique_id())
+	var bank_resources = inventory.get_player_resources(0)
 	for i in resource_dict:
 		resource_count[resource_dict[i]].text = str(player_resources[resource_dict[i]])
+		bank_resource_count[resource_dict[i]].text = str(bank_resources[resource_dict[i]])
 
 func _on_dice_button_pressed() -> void:
 	k1 = randi() % 6 + 1
@@ -54,3 +67,11 @@ func _on_hide_button_toggled(toggled_on: bool) -> void:
 		resources_container_tween.tween_property(resources_container,"position",resources_container_pos ,1)
 	else:
 		resources_container_tween.tween_property(resources_container,"position",resources_container_pos + Vector2(0,50), 1)
+
+
+func _on_bank_button_toggled(toggled_on: bool) -> void:
+	var bank_container_tween = create_tween()
+	if toggled_on:
+		bank_container_tween.tween_property(bank_container,"position",bank_container_pos ,1)
+	else:
+		bank_container_tween.tween_property(bank_container,"position",bank_container_pos + Vector2(110,0), 1)
